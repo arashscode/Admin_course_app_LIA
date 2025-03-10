775629ad228886057cadcc52b81989f1721469f2
Martin jernström <martin.jernstrom1@gmail.com>
2025-03-10 11:10:40 +0100

    removed downloadsfile

--- Downloads/Admin_course_app_LIA-master/Admin_course_app_LIA-master/src/main/java/com/aldinalj/admin_course_app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aldinalj.admin_course_app.repository;
-
-import com.aldinalj.admin_course_app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
