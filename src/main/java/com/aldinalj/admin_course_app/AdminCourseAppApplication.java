612bae92c437af78c64883530a16f4018b669951
Martin jernström <martin.jernstrom1@gmail.com>
2025-03-17 12:18:05 +0100

    webhook

--- src/main/java/com/aldinalj/admin_course_app/AdminCourseAppApplication.java
+++ src/main/java/com/aldinalj/admin_course_app/AdminCourseAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminCourseAppApplication {
 
+    //this is a comment to see if webhook works
     public static void main(String[] args) {
         SpringApplication.run(AdminCourseAppApplication.class, args);
     }
