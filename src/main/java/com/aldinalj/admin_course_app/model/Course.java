810f84a1df944a947134ea9177d049f78712a4e7
Arash Sohrevardi <153449366+arashscode@users.noreply.github.com>
2025-03-19 11:45:59 +0100

    alernated coursdto order, no id needed, constructors.

--- src/main/java/com/aldinalj/admin_course_app/model/Course.java
+++ src/main/java/com/aldinalj/admin_course_app/model/Course.java
@@ -1,37 +1,40 @@
 package com.aldinalj.admin_course_app.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "courses", indexes = {
+        @Index(name = "idx_course_name", columnList = "name")  // 🔥 Skapar ett index på "name"
+})
 public class Course {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Changed to IDENTITY for compability MySQL/H2 AUTO_INCREMENT on id
     private Long id;
 
-    @NotBlank
     private String name;
-
-    @NotBlank
     private String code;
-
-    @FutureOrPresent
     private LocalDate startDate;
-
-    @Future
     private LocalDate endDate;
-
-    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Category category;
     private String description;
 
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private Category category;
+
+
+    public Course() {}
+
+    // Constructor for instansation
+    public Course(String name, String code, LocalDate startDate, LocalDate endDate, String description, Category category) {
+        this.name = name;
+        this.code = code;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.category = category;
+        this.description = description;
+
+    }
 
     public Long getId() {
         return id;
