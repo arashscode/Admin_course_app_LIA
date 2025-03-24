81a342091e0867f17d03e34a2119ed19fc2f4561
Sertaan <sertan.keskin@stud.sti.se>
2025-03-24 10:43:56 +0100

    update SecurityConfig.java

--- src/main/java/com/aldinalj/admin_course_app/config/SecurityConfig.java
+++ src/main/java/com/aldinalj/admin_course_app/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.aldinalj.admin_course_app.config;
 
 import com.aldinalj.admin_course_app.service.UserAuthService;
-import com.aldinalj.admin_course_app.service.UserService;
 import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +10,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsService;  //From frontend
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 
 @Configuration
 @EnableWebSecurity
@@ -31,38 +28,34 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable()) // Activate in production
                 .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/login" ).permitAll()
-
-
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/login").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST,"/api/**" ).hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET,"/api/**" ).hasAnyRole("USER", "ADMIN")
-                        .requestMatchers(HttpMethod.PUT,"/api/**" ).hasAnyRole("USER", "ADMIN")
-                        .anyRequest().authenticated())
-                .sessionManagement(session -> session
+                        .requestMatchers(HttpMethod.POST,"/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/api/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session  //code from frontend
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 );
         http
+                //.formLogin(Customizer.withDefaults()) //This i original from backend, not in updated request from frontend.
                 .logout(logout -> logout
-                        .logoutUrl("/logout")  // ✅ Logout måste göras med en POST-request till /logout
+                        .logoutUrl("/logout")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 );
 
-
-
         return http.build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
 
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
